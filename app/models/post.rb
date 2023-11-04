9cfba69e0f5499078b65d8c4af607f6a29a6d338
maximumry <144888797+maximumry@users.noreply.github.com>
2023-11-04 20:14:08 +0900

    動画投稿の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
-  has_one_attached :video
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :industry
   belongs_to :user
+  has_one_attached :video
+
+  validates :industry_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :description, presence: true
+  validates :title, presence: true
 end
