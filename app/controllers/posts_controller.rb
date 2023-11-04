cab190f2aba9ae2df06f68f0345af207881b0f98
maximumry <teapots-thorium.07@icloud.com>
2023-11-04 20:14:08 +0900

    動画投稿の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :description, :video).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :description, :video, :industry_id).merge(user_id: current_user.id)
   end
 end
