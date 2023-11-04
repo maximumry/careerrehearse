f71ca2829f428055602457145fd105fef67ae374
maximumry <teapots-thorium.07@icloud.com>
2023-11-04 18:05:45 +0900

    userモデルのテスト実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
-  validates :username, presence: true
+
+  validates :password, length: {minimum: 16, message: "is too short (minimum is 16 characters)"}, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
+  validates :username, presence: true, length: {maximum: 15, message: "is too long (maximum is 15 characters)"}, format: { with: /\A[a-zA-Zぁ-んァ-ヶ一-龥々ー]+\z/, message: "only accepts English letters, Hiragana, Katakana, and Kanji" }
 end
