ebe6bb7d0d2cd006d32f63f3404d47425f85f6a6
maximumry <144888797+maximumry@users.noreply.github.com>
2023-11-05 17:09:03 +0900

    postモデルのテスト実施

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,10 +16,36 @@ RSpec.describe Post, type: :model do
         @post.valid?
         expect(@post.errors.full_messages).to include("Video can't be blank")
       end
-      it 'が空だと投稿できない' do
-        @post.video = nil
+      it 'industry_idが空だと投稿できない' do
+        @post.industry_id = ""
         @post.valid?
-        expect(@post.errors.full_messages).to include("Video can't be blank")
+        expect(@post.errors.full_messages).to include("Industry can't be blank")
       end
+      it 'industry_idが___だと投稿できない' do
+        @post.industry_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Industry can't be blank")
+      end
+      it 'タイトルが空だと投稿できない' do
+        @post.title = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'タイトルが50文字以上だと投稿できない' do
+        @post.title = "abcdefghijklmnopqrstuvwkyzabcdefghijklmnopqrstuvwxyzabcdefghijklmn"
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title is too long (maximum is 50 characters)")
+      end
+      it '動画の詳細が空だと投稿できない' do
+        @post.description = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'user_idが紐づいていないと投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("User must exist")
+      end
+    end
   end
 end
