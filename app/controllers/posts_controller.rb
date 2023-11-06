79f7a08a92d9a46c82867400495f57cd787586a0
maximumry <144888797+maximumry@users.noreply.github.com>
2023-11-06 11:54:38 +0900

    投稿の編集の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_match, only: :edit
   def index
     @posts = Post.order("created_at DESC")
   end
@@ -21,8 +22,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :description, :video, :industry_id).merge(user_id: current_user.id)
+  end
+
+  def user_match
+    @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to root_path
+    end
   end
 end
