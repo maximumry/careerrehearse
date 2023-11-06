f23095ecf7e7aa1a689a9824987953982e0187f3
maximumry <144888797+maximumry@users.noreply.github.com>
2023-11-06 12:26:25 +0900

    すべてのアクションをログイン状態でできる様に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
   before_action :user_match, only: [:edit, :destroy]
   def index
     @posts = Post.order("created_at DESC")
