6a1dfa0075fcbc44a83a8780290c88a013ab46c6
maximumry <max_squis0776@icloud.com>
2023-11-08 21:23:05 +0900

    titleとdescriptionカラムの検索実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
     resource :likes, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show do
     resource :relationships, only: [:create, :destroy]
