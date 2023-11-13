03887c5991bba4be04a7b630d33ab62a25e13b74
maximumry <maximum.river1211@gmail.com>
2023-11-13 19:13:22 +0900

    フォーム内で複数検索できるよう条件分岐

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,4 +26,8 @@ class Post < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["title", "description", "industry_id"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
