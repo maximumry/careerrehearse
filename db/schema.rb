e395acfc8eb3645d79d26f296b4df1b998156aed
maximumry <maximum.river1211@gmail.com>
2023-11-19 19:08:21 +0900

    snsアカウント用のテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_14_095941) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_095251) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -86,6 +86,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_095941) do
     t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
+  create_table "sns_credentials", charset: "utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "tags", charset: "utf8", force: :cascade do |t|
     t.string "tag_name", null: false
     t.datetime "created_at", null: false
@@ -114,4 +123,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_095941) do
   add_foreign_key "post_tag_relations", "posts"
   add_foreign_key "post_tag_relations", "tags"
   add_foreign_key "posts", "users"
+  add_foreign_key "sns_credentials", "users"
 end
