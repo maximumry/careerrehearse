56fd0fc59e227aa069bcd491647d4ca44c51d959
maximumry <maximum.river1211@gmail.com>
2023-11-20 19:21:10 +0900

    パスワードなしでログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,15 +26,18 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = User.where(email: auth.info.email).first_or_initialize(
       username: auth.info.name,
       email: auth.info.email
     )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
     if user.persisted?
       sns.user = user
       sns.save
     end
-    user
+    { user: user, sns: sns }
   end
 
   validates :password, length: {minimum: 16, message: "is too short (minimum is 16 characters)"}, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
