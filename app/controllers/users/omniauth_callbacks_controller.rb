99eafae45ddf137f16f68ceb4c842a6a3ba957ee
maximumry <max_squis0776@icloud.com>
2023-11-20 19:00:25 +0900

    snsアカウントで新規登録の実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
    private
   
    def authorization
-     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      render template: 'devise/registrations/new'
+    end
    end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
