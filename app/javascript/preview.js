3980d281f3d7c78dfeb17772dd6b9e011c5fcdaa
maximumry <maximum.river1211@gmail.com>
2023-11-22 14:35:52 +0900

    jsファイルの作成

---
+++ app/javascript/preview.js
@@ -0,0 +1,6 @@
+document.getElementById('turbo:load', function() {
+  const postForm = document.getElementById("new_post");
+
+  if(!postForm) return null;;
+  console.log("preview.jsが読み込まれた");
+});
