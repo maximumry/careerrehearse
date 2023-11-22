1394788c218caf868cf47c8be262612d51922be0
maximumry <max_squis0776@icloud.com>
2023-11-22 14:35:52 +0900

    jsファイルの作成

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-# pin "tag", to: "tag.js"
+pin "tag", to: "tag.js"
+pin "preview", to: "preview.js"
