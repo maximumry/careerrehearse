e2f334897770e9e287c899d6cb2384b45a1ae3e0
maximumry <maximum.river1211@gmail.com>
2023-11-26 10:52:44 +0900

    タグ機能の修正

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -10,26 +10,22 @@ class PostForm
   end
 
   def save
-    Post.create(title: title, video: video, description: description, industry_id: industry_id, user_id: user_id)
-    if tag_name.present?
-      tag = Tag.where(tag_name: tag_name).first_or_initialize
-      tag.save
-    else
-      PostTagRelation.create(post_id: post.id, tag_id: tag.id)
-    end
+  post = Post.create(title: title, video: video, description: description, industry_id: industry_id, user_id: user_id)
+
+  if tag_name.present?
+    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    tag.save
+    PostTagRelation.create(post_id: post.id, tag_id: tag.id)
   end
+end
 
   def update(params, post)
-    #一度タグの紐付けを消す
     post.post_tag_relations.destroy_all
 
-    #paramsの中のタグの情報を削除。同時に、返り値としてタグの情報を変数に代入
     tag_name = params.delete(:tag_name)
 
-    #もしタグの情報がすでに保存されていればインスタンスを取得、無ければインスタンスを新規作成
     tag = Tag.where(tag_name: tag_name).first_or_initialize if tag_name.present?
 
-    #タグを保存
     tag.save if tag_name.present?
     post.update(params)
     PostTagRelation.create(post_id: post.id, tag_id: tag.id) if tag_name.present?
