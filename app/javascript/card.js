e3ca6ba70778fbdf557269346007ed226444411f
maximumry <max_squis0776@icloud.com>
2023-12-11 20:02:52 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -29,25 +29,6 @@ const pay = () => {
 
 window.addEventListener("load", pay);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const pay = () => {
-//   const publicKey = gon.public_key
-//   const payjp = Payjp(publicKey)
-//   const elements = payjp.elements();
-//   const numberElement = elements.create('cardNumber');
-//   const expiryElement = elements.create('cardExpiry');
 //   const cvcElement = elements.create('cardCvc');
 
 //   numberElement.mount('#number-form');
