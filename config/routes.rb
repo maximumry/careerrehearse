3497b8c83fef281dc9725a148d3b4df5508b3087
maximumry <max_squis0776@icloud.com>
2023-12-11 20:02:24 +0900

    tokenの確認

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   end
   resources :users, only: :show do
     resource :relationships, only: [:create, :destroy]
-    resources :transactions, only: :index
+    resources :transactions, only: [:index, :create]
   end
 end
