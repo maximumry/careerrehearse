bf61de7998df346e72e091ddf1b81e403042e435
maximumry <maximum.river1211@gmail.com>
2023-12-11 20:03:12 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -28,30 +28,3 @@ const pay = () => {
 };
 
 window.addEventListener("load", pay);
-
-//   const cvcElement = elements.create('cardCvc');
-
-//   numberElement.mount('#number-form');
-//   expiryElement.mount('#expiry-form');
-//   cvcElement.mount('#cvc-form');
-//   const form = document.getElementById('charge-form')
-//   form.addEventListener("submit", (e) => {
-//     payjp.createToken(numberElement).then(function (response) {
-//       if (response.error) {
-//       } else {
-//         const token = response.id;
-//         const renderDom = document.getElementById("charge-form");
-//         const tokenObj = `<input value=${token} name='token' type="hidden">`;
-//         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-//       }
-//       numberElement.clear();
-//       expiryElement.clear();
-//       cvcElement.clear();
-//       document.getElementById("charge-form").submit();
-//     });
-//     e.preventDefault();
-//   });
-// };
-
-// window.addEventListener("turbo:load", pay);
-// window.addEventListener("turbo:render", pay);
