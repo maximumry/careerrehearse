9d1c0d35ff87285c7bc78a7a2d8f749fcda46c7c
maximumry <maximum.river1211@gmail.com>
2023-12-12 19:42:25 +0900

    リファクタリング

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,15 +5,11 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.find(params[:user_id])
     @transaction = Transaction.new(transaction_params)
     if @transaction.valid?
-      Payjp.api_key = "sk_test_35285d916314b9eb100df281"
-      Payjp::Charge.create(
-        amount: transaction_params[:price], #送金の値段
-        card: transaction_params[:token], #カードトークン
-        currency: "jpy" #通貨の種類
-      )
+      api_pay
       @transaction.save
       redirect_to user_path(@transaction.user)
     else
@@ -24,5 +20,14 @@ class TransactionsController < ApplicationController
   private
   def transaction_params
     params.require(:transaction).permit(:price, :text).merge(token: params[:token])
+  end
+
+  def api_pay
+    Payjp.api_key = "sk_test_35285d916314b9eb100df281"
+      Payjp::Charge.create(
+        amount: transaction_params[:price], #送金の値段
+        card: transaction_params[:token], #カードトークン
+        currency: "jpy" #通貨の種類
+      )
   end
 end
