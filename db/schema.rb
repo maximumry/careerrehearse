4aca98bc08fd48ffa09d68885dd8fca99e93fc0e
maximumry <max_squis0776@icloud.com>
2023-12-21 15:32:45 +0900

    transactionsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_020846) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_062948) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -106,6 +106,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_020846) do
     t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -131,4 +133,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_020846) do
   add_foreign_key "post_tag_relations", "tags"
   add_foreign_key "posts", "users"
   add_foreign_key "sns_credentials", "users"
+  add_foreign_key "transactions", "users"
 end
